10ebb2cd2419de7d01c0104ecf41e869457096f6
Jeremy MATHON <jerem@hivebrite.com>
2020-05-16 18:33:34 +0200

    Feature(parse): Add equalizer options + creation interface for ParseOptions

--- qs.ts
+++ qs.ts
@@ -1,11 +1,19 @@
+interface ParseOptions {
+  sep?: string;
+  eq?: string;
+}
+
 export class qs {
-  static parse(query: string, sep: string = "&"): Object {
+  static parse(
+    query: string,
+    { sep = "&", eq = "=" }: ParseOptions = {}
+  ): Record<string, string> {
     let segmentation = query.split(sep);
 
     return segmentation.reduce(
       (result: Record<string, string>, str: string) => {
-        let key: string = str.split("=")[0];
-        let value: string = str.split("=")[1];
+        let key: string = str.split(eq)[0];
+        let value: string = str.split(eq)[1];
         let return_value: Record<string, string> = {};
 
         return_value[key] = value;
